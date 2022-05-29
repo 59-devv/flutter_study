3825904d68b65d3119f859dc95a7dd6d530febb1
59 <5.9.devv@gmail.com>
2022-05-29 17:58:05 +0900

    complete xylophone-project

--- xylophone-flutter-master/lib/main.dart
+++ xylophone-flutter-master/lib/main.dart
@@ -1,14 +1,45 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(
+      XylophoneApp(),
+    );
 
 class XylophoneApp extends StatelessWidget {
+  AudioCache audioPlayer = AudioCache();
+
+  Expanded makeColoredBox(Color color, int number) {
+    return Expanded(
+      child: ColoredBox(
+        color: color,
+        child: TextButton(
+          onPressed: () {
+            playSound(number);
+          },
+        ),
+      ),
+    );
+  }
+
+  void playSound(int number) => audioPlayer.play('note$number.wav');
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              makeColoredBox(Colors.red, 1),
+              makeColoredBox(Colors.orange, 2),
+              makeColoredBox(Colors.yellow, 3),
+              makeColoredBox(Colors.green, 4),
+              makeColoredBox(Colors.green.shade800, 5),
+              makeColoredBox(Colors.blue, 6),
+              makeColoredBox(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
