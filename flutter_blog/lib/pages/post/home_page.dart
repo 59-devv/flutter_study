01e782e771f245a2eac0f59a4821e40853845c01
GHOH <ghoh@mediance.co.kr>
2022-07-12 13:27:26 +0900

    listview

--- flutter_blog/lib/pages/post/home_page.dart
+++ flutter_blog/lib/pages/post/home_page.dart
@@ -1,13 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/components/size.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Page"),
+    return Scaffold(
+      drawer: _drawer(context),
+      appBar: AppBar(),
+      body: ListView.separated(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return ListTile(
+              onTap: (){},
+              title: const Text("ㅁㄴㅇㄹ"),
+            );
+          },
+        separatorBuilder: (context, index) {
+            return const Divider();
+        },
+          )
+    );
+  }
+
+  Widget _drawer(BuildContext context) {
+    return Container(
+      width: getDrawerWidth(context),
+      height: double.infinity,
+      color: Colors.white,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                  onPressed: (){ },
+                  child: const Text(
+                      "회원 정보 보기",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.bold
+                      ),
+                  ),
+              ),
+              const Divider(),
+              TextButton(
+                onPressed: (){ },
+                child: const Text(
+                  "로그아웃",
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
       ),
     );
   }
