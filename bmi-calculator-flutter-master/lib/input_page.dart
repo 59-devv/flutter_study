0655bbba09b4eb31a7f4e18641e0ebfe0f5202a4
GHOH <ghoh@mediance.co.kr>
2022-08-01 13:21:01 +0900

    0801

--- bmi-calculator-flutter-master/lib/input_page.dart
+++ bmi-calculator-flutter-master/lib/input_page.dart
@@ -9,11 +9,57 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text2'),
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('BMI CALCULATOR'),
+          backgroundColor: Color(0xFF0A0E21),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(color: Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF1D1E33)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+
+  ReusableCard({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
--- bmi-calculator-flutter-master/lib/main.dart
+++ bmi-calculator-flutter-master/lib/main.dart
@@ -7,15 +7,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-        colorScheme: ThemeData().colorScheme.copyWith(
-              primary: Color(0xFF0A0E21),
-              secondary: Colors.red,
-            ),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.white),
-        ),
       ),
       home: InputPage(),
     );
